4a12b4fe1c66568025154727214ddabb7e335283
BESINA-star <soniabesina806@gmail.com>
2026-05-17 13:49:18 +0100

    sevices created

---
+++ services/recipeService.js
@@ -0,0 +1,14 @@
+const Recipe = require('../models/recipeModel');
+
+const createRecipe = async (data) => {
+  if (data.cookingTime <= 0) {
+    throw new Error('Cooking time must be a positive number');
+  }
+
+  const recipe = await Recipe.create(data);
+  return recipe;
+};
+
+module.exports = {
+  createRecipe
+};
