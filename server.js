db63865b9ce3e0d0c486a8e2a3e70218a9b8969a
BESINA-star <soniabesina806@gmail.com>
2026-05-17 13:41:12 +0100

    created model

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+// Connect Database
+connectDB();
+
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('My First API');
+  res.send('My first API');
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
